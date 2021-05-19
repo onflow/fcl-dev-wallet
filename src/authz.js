7abe9ff07868de3527b9600c1b317e0edaa07969
qvvg <1102494+orodio@users.noreply.github.com>
2021-05-19 12:49:46 -0700

    Update authz.js

--- src/authz.js
+++ src/authz.js
@@ -4,7 +4,7 @@ import * as fcl from "@onflow/fcl"
 export async function authz(account) {
   return {
     ...account,
-    tempId: "SERVICE_ACCOUNT",
+    tempId: "SERVICE_ACCOUNT", // this is a special case, when in doubt you probably want something like tempId: `${address}-${keyId}`
     addr: fcl.sansPrefix(process.env.FLOW_ACCOUNT_ADDRESS),
     keyId: Number(process.env.FLOW_ACCOUNT_KEY_ID),
     signingFunction: data => ({
