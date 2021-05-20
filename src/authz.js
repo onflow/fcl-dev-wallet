7e05220ad25322706652fcddfd41ad341212938f
qvvg <1102494+orodio@users.noreply.github.com>
2021-05-20 08:50:53 -0700

    Update authz.js

--- src/authz.js
+++ src/authz.js
@@ -22,7 +22,7 @@ export async function authz(account) {
     addr: fcl.sansPrefix(process.env.FLOW_ACCOUNT_ADDRESS), // eventually it wont matter if this address has a prefix or not, sadly :'( currently it does matter.
     keyId: Number(process.env.FLOW_ACCOUNT_KEY_ID), // must be a number
     signingFunction: signable => ({
-      addr: fcl.withPrefix(process.env.FLOW_ACCOUNT_ADDRESS), // must match the address that requested the signature
+      addr: fcl.withPrefix(process.env.FLOW_ACCOUNT_ADDRESS), // must match the address that requested the signature, but with a prefix
       keyId: Number(process.env.FLOW_ACCOUNT_KEY_ID), // must match the keyId in the account that requested the signature
       signature: sign(process.env.FLOW_ACCOUNT_PRIVATE_KEY, signable.message), // signable.message |> hexToBinArray |> hash |> sign |> binArrayToHex
       // if you arent in control of the transaction that is being signed we recommend constructing the 
