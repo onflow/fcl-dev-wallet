f0625c0afc8aff11a31bd985957d2187b4883a84
Peter Siemens <peterjsiemens@gmail.com>
2021-07-11 21:24:12 -0700

    Increase gas limit on init

--- pages/api/init.js
+++ pages/api/init.js
@@ -85,7 +85,7 @@ const init = async () => {
       fcl.proposer(authz),
       fcl.payer(authz),
       fcl.authorizations([authz]),
-      fcl.limit(90),
+      fcl.limit(200),
     ]).then(fcl.decode)
     console.log("TX", txId)
     const txStatus = await fcl.tx(txId).onceSealed()
