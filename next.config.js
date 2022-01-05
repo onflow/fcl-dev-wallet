677df50a1e6671c7f70e2fae07cc8f77e2487de4
Greg Santos <greg.santos@dapperlabs.com>
2022-01-05 11:51:45 -0800

    Add refresh service, cors, and baseUrl

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          {key: "Access-Control-Allow-Credentials", value: "true"},
+          {key: "Access-Control-Allow-Origin", value: "*"},
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ]
+  },
   webpack: (config, _options) => {
     config.module.rules.push({
       test: /\.cdc/,
@@ -15,6 +36,7 @@ module.exports = {
   },
   publicRuntimeConfig: {
     flowAccountAddress: process.env.FLOW_ACCOUNT_ADDRESS,
+    baseUrl: process.env.BASE_URL,
     avatarUrl: process.env.FLOW_AVATAR_URL,
     contractFungibleToken: process.env.CONTRACT_FUNGIBLE_TOKEN,
     contractFlowToken: process.env.CONTRACT_FLOW_TOKEN,
