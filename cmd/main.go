9f94b4c02325b44a93011e2bbbbb7f7cbd41eb3b
10thfloor <remember.mackenzie@gmail.com>
2022-03-03 15:49:42 -0800

    Update emulator address in cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	wallet "github.com/onflow/fcl-dev-wallet"
 )
 
@@ -10,7 +11,7 @@ func main() {
 		Address:    "f8d6e0586b0a20c7",
 		PrivateKey: "68ee617d9bf67a4677af80aaca5a090fcda80ff2f4dbc340e0e36201fa1f1d8c",
 		PublicKey:  "9cd98d436d111aab0718ab008a466d636a22ac3679d335b77e33ef7c52d9c8ce47cf5ad71ba38cedd336402aa62d5986dc224311383383c09125ec0636c0b042",
-		AccessNode: "localhost:3569",
+		AccessNode: "localhost:8080",
 	})
 	if err != nil {
 		panic(err)
