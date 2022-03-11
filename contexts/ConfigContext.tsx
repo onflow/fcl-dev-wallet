2ab108185e37868d175782a0bc70982d575e9d14
10thfloor <remember.mackenzie@gmail.com>
2022-03-11 13:42:14 -0800

    Better warning in CI.

--- contexts/ConfigContext.tsx
+++ contexts/ConfigContext.tsx
@@ -22,9 +22,10 @@ export async function fetchConfigFromAPI(): Promise<RuntimeConfig> {
   return fetch("http://localhost:8701/api/")
     .then(res => res.json())
     .catch(e => {
-      console.error(
-        `Failed to fetch config from API. Are you sure the dev-web-server is running on port 8701?
-            ${e}
+      console.log(
+        `Warning: Failed to fetch config from API. 
+         If you see this warning during CI you can ignore it.
+         ${e}
           `
       )
 
