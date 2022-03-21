27ea8c6f3ec347f0545ce15d26317e70721b9f53
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2022-03-21 12:53:41 +0100

    small refactor

--- server.go
+++ server.go
@@ -23,15 +23,15 @@ type Config struct {
 	AccessNode string `json:"flowAccessNode"`
 }
 
-type Server struct {
+type server struct {
 	http   *http.Server
 	config *Config
 }
 
 // NewHTTPServer returns a new wallet server listening on provided port number.
-func NewHTTPServer(port uint, config *Config) (*Server, error) {
+func NewHTTPServer(port uint, config *Config) (*server, error) {
 	mux := http.NewServeMux()
-	server := &Server{
+	srv := &server{
 		http: &http.Server{
 			Addr:    fmt.Sprintf(":%d", port),
 			Handler: mux,
@@ -39,41 +39,47 @@ func NewHTTPServer(port uint, config *Config) (*Server, error) {
 		config: config,
 	}
 
-	zipContent, _ := bundle.ReadFile(bundleZip)
-	zipFS, _ := zip.NewReader(bytes.NewReader(zipContent), int64(len(zipContent)))
-	rootFS := http.FS(zipFS)
+	mux.HandleFunc("/api/", configHandler(srv))
+	mux.HandleFunc("/", devWalletHandler())
 
-	// handle /api endpoint returning the configuration
-	mux.HandleFunc("/api/", func(w http.ResponseWriter, r *http.Request) {
+	return srv, nil
+}
+
+// configHandler handles config endpoints
+func configHandler(server *server) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		err := json.NewEncoder(w).Encode(server.config)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 		}
-	})
+	}
+}
 
-	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+// devWalletHandler handles endpoints to exported static html files
+func devWalletHandler() func(writer http.ResponseWriter, request *http.Request) {
+	zipContent, _ := bundle.ReadFile(bundleZip)
+	zipFS, _ := zip.NewReader(bytes.NewReader(zipContent), int64(len(zipContent)))
+	rootFS := http.FS(zipFS)
+
+	return func(writer http.ResponseWriter, request *http.Request) {
 		path := strings.TrimPrefix(request.URL.Path, "/")
-
-		// if not found and there is no / suffix (which would mean index.html) then try adding .html for file
-		if path != "" {
+		if path != "" { // api requests don't include .html so that needs to be added
 			if _, err := zipFS.Open(path); err != nil {
 				path = fmt.Sprintf("%s.html", path)
 			}
 		}
 
-		request.URL.Path = path // overwrite path for file server handler
+		request.URL.Path = path
 		http.FileServer(rootFS).ServeHTTP(writer, request)
-	})
-
-	return server, nil
+	}
 }
 
-func (s *Server) Start() error {
+func (s *server) Start() error {
 	return s.http.ListenAndServe()
 }
 
-func (s *Server) Stop() {
+func (s *server) Stop() {
 	_ = s.http.Shutdown(context.Background())
 }
