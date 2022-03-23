6e0d6bd8ddb23eb3ccebf861199b7aea483a3776
10thfloor <remember.mackenzie@gmail.com>
2022-03-23 11:57:28 -0700

    Adds configs for local dev.

--- contexts/ConfigContext.tsx
+++ contexts/ConfigContext.tsx
@@ -1,4 +1,5 @@
 import React, {createContext, useEffect, useState} from "react"
+import getConfig from "next/config"
 
 interface RuntimeConfig {
   flowAccountAddress: string
@@ -8,23 +9,30 @@ interface RuntimeConfig {
   flowAccessNode: string
 }
 
+const {publicRuntimeConfig} = getConfig()
+
 const defaultConfig = {
-  flowAccountAddress: "",
-  flowAccountPrivateKey: "",
-  flowAccountPublicKey: "",
-  flowAccountKeyId: "",
-  flowAccessNode: "",
+  flowAccountAddress: publicRuntimeConfig.flowAccountAddress || "",
+  flowAccountPrivateKey: publicRuntimeConfig.flowAccountPrivateKey || "",
+  flowAccountPublicKey: publicRuntimeConfig.flowAccountPublicKey || "",
+  flowAccountKeyId: publicRuntimeConfig.flowAccountKeyId || "",
+  flowAccessNode: publicRuntimeConfig.flowAccessNode || "",
 }
 
 export const ConfigContext = createContext<RuntimeConfig>(defaultConfig)
 
 export async function fetchConfigFromAPI(): Promise<RuntimeConfig> {
+  if (publicRuntimeConfig.isLocal) {
+    return defaultConfig
+  }
+
   return fetch("http://localhost:8701/api/")
     .then(res => res.json())
     .catch(e => {
       console.log(
         `Warning: Failed to fetch config from API. 
          If you see this warning during CI you can ignore it.
+         Returning default config.
          ${e}
           `
       )
--- next.config.js
+++ next.config.js
@@ -29,8 +29,13 @@ module.exports = {
     return config
   },
   publicRuntimeConfig: {
+    isLocal: process.env.APP_ENV === "local",
     flowInitAccountsNo: process.env.FLOW_INIT_ACCOUNTS,
     flowAccountKeyId: process.env.FLOW_ACCOUNT_KEY_ID,
+    flowAccountAddress: process.env.FLOW_ACCOUNT_ADDRESS,
+    flowAccountPrivateKey: process.env.FLOW_ACCOUNT_PRIVATE_KEY,
+    flowAccountPublicKey: process.env.FLOW_ACCOUNT_PUBLIC_KEY,
+    flowAccessNode: process.env.FLOW_ACCESS_NODE,
     baseUrl: process.env.BASE_URL,
     avatarUrl: process.env.FLOW_AVATAR_URL,
     contractFungibleToken: process.env.CONTRACT_FUNGIBLE_TOKEN,
