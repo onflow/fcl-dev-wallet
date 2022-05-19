6fad80446eec299a64296f6c40b46796f5aba820
Greg Santos <greg.santos@dapperlabs.com>
2022-05-18 17:32:05 -0700

    Add conditional to theme.colors

--- components/InfoIcon.tsx
+++ components/InfoIcon.tsx
@@ -12,7 +12,7 @@ function InfoIcon({active}: {active: boolean}) {
       viewBox="0 0 22 22"
     >
       <path
-        stroke={active ? theme.colors.green : theme.colors.gray[500]}
+        stroke={active ? theme.colors?.green : theme.colors?.gray[500]}
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M11 21c5.523 0 10-4.477 10-10S16.523 1 11 1 1 5.477 1 11s4.477 10 10 10zM11 15v-4M11 7v-.5"
