7e457f91cf83705e4c14cc63d570dc32c8b052ab
justin <328965+justinbarry@users.noreply.github.com>
2022-06-17 16:51:36 -0700

    Set AccessNode port to 8888

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ func main() {
 		PrivateKey:            "f8e188e8af0b8b414be59c4a1a15cc666c898fb34d94156e9b51e18bfde754a5",
 		PublicKey:             "6e70492cb4ec2a6013e916114bc8bf6496f3335562f315e18b085c19da659bdfd88979a5904ae8bd9b4fd52a07fc759bad9551c04f289210784e7b08980516d2",
 		AccountKeyId:          "0",
-		AccessNode:            "http://localhost:8080",
+		AccessNode:            "http://localhost:8888",
 		BaseUrl:               "http://localhost:8701",
 		ContractFungibleToken: "0xee82856bf20e2aa6",
 		ContractFlowToken:     "0x0ae53cb6e3f42a79",
