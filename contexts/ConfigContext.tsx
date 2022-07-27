4fd0de69debbaa3efa58c6fcbf26a99d2e432123
Louis Guitton <louisguitton93@gmail.com>
2022-07-27 14:42:16 +0200

    Fix 404 error
    Co-authored-by:
    Valdrin Trena
    <valdrin-t@users.noreply.github.com>

--- contexts/ConfigContext.tsx
+++ contexts/ConfigContext.tsx
@@ -38,19 +38,22 @@ async function getConfig(): Promise<RuntimeConfig> {
     return defaultConfig
   }
 
-  const result = await fetch("http://localhost:8701/api/")
-    .then(res => res.json())
-    .catch(e => {
-      console.log(
-        `Warning: Failed to fetch config from API. 
-         If you see this warning during CI you can ignore it.
-         Returning default config.
-         ${e}
-          `
-      )
-      return defaultConfig
-    })
-
+  const result = {
+    flowAccountAddress: "0xf8d6e0586b0a20c7",
+    flowAccountPrivateKey:
+      "f8e188e8af0b8b414be59c4a1a15cc666c898fb34d94156e9b51e18bfde754a5",
+    flowAccountPublicKey:
+      "6e70492cb4ec2a6013e916114bc8bf6496f3335562f315e18b085c19da659bdfd88979a5904ae8bd9b4fd52a07fc759bad9551c04f289210784e7b08980516d2",
+    flowAccountKeyId: "0",
+    flowAccessNode: "http://emulator:8888",
+    baseUrl: "http://localhost:8701",
+    contractFungibleToken: "0xee82856bf20e2aa6",
+    contractFlowToken: "0x0ae53cb6e3f42a79",
+    contractFUSD: "0xf8d6e0586b0a20c7",
+    contractFCLCrypto: "0xf8d6e0586b0a20c7",
+    avatarUrl: "",
+    flowInitAccountsNo: 0,
+  }
   return result
 }
 
